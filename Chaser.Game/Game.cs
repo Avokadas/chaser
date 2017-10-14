b5052832516e262a72038e9a734ed60427b8040d
Matas Jurkšaitis <iammatjuu@gmail.com>
2017-10-14 17:47:37 +0300

    Moved logic to renderengine

---
+++ Chaser.Game/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chaser.Game
+{
+
+    public class GameStateSingleton
+    {
+        private static GameStateSingleton _instance;
+
+        private GameStateSingleton()
+        {
+        }
+
+        public static GameStateSingleton Instance => _instance ?? (_instance = new GameStateSingleton());
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Chaser.UI;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
@@ -7,58 +8,14 @@ namespace Chaser
 {
     class Program
     {
-        private static ContextSettings _context = new ContextSettings();
-        private static RenderWindow _window;
-        private static Sprite dogSprite = new Sprite(new Texture("Assets/doge.jpg"));
+        private static RenderingEngine _renderingEngine = new RenderingEngine();
 
         static void Main()
         {
-            _window = new RenderWindow(new VideoMode(1024, 768), "Chaser", Styles.Default, _context);
-            _window.Closed += Window_Closed;
-            _window.KeyPressed += Window_KeyPressed;
-            dogSprite.Position = new Vector2f(100, 100);
-            dogSprite.TextureRect = new IntRect(100,100,100,100);
-
-            while (_window.IsOpen)
+            while (_renderingEngine.Window.IsOpen)
             {
-                _window.DispatchEvents();
-                _window.Clear();
-                dogSprite.Draw(_window, RenderStates.Default);
-                _window.Display();
+                _renderingEngine.RenderGameState();
             }
-        }
-
-        private static void Window_KeyPressed(object sender, KeyEventArgs e)
-        {
-            if (e.Code == Keyboard.Key.Escape)
-            {
-               _window.Close();
-            }
-            if (e.Code == Keyboard.Key.W)
-            {
-                var oldPos = dogSprite.Position;
-                dogSprite.Position = new Vector2f(oldPos.X, oldPos.Y + 10);
-            }
-            if (e.Code == Keyboard.Key.A)
-            {
-                var oldPos = dogSprite.Position;
-                dogSprite.Position = new Vector2f(oldPos.X - 10, oldPos.Y);
-            }
-            if (e.Code == Keyboard.Key.S)
-            {
-                var oldPos = dogSprite.Position;
-                dogSprite.Position = new Vector2f(oldPos.X, oldPos.Y - 10);
-            }
-            if (e.Code == Keyboard.Key.D)
-            {
-                var oldPos = dogSprite.Position;
-                dogSprite.Position = new Vector2f(oldPos.X + 10, oldPos.Y);
-            }
-        }
-
-        private static void Window_Closed(object sender, EventArgs e)
-        {
-            _window.Close();
         }
     }
 }
